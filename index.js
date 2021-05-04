9263bdcd82bbab8b3923b4d07c605abf8837d993
Mira Chami <miramc149@gmail.com>
2021-05-04 20:01:09 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ function createEmployeeRecord(firstName,lastName,title,payPerhour)
   let payPerhour = this.payPerhour;
   let timeInEvents = this.timeInEvents;
   let timeOutEvents = this.timeOutEvents;
-  let person{
+  let person = {
     firstName,
     lastName,
     title,
