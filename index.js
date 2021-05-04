8fbb612fa6f75e6020f597e82ad5b730393c88e9
Mira Chami <miramc149@gmail.com>
2021-05-04 19:55:27 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
 // Your code here
+function createEmployeeRecord(arr)
+{
+  let firstName = this.firstName;
+  let lastName = this.familyName;
+  let title = this.title;
+  let pay = this.payPerhour;
+  
+}
+function createEmployeeRecords(arr)
+{}
